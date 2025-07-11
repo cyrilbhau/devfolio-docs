895c464953ea6647af357474e68374ac21d582f3
Vee <vee1912566@gmail.com>
2025-07-11 17:17:48 +0530

    chore: fix layout

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -11,22 +11,21 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        Devfolio
+        <img
+          src="/assets/Devfolio - Normal.png" 
+          alt="Devfolio" 
+          width={120} 
+          height={24}
+          className="h-6"
+        />
+        
       </>
     ),
   },
   links: [
     {
       text: 'Documentation',
-      url: '/docs',
+      url: '/docs/guide',
       active: 'nested-url',
     },
   ],

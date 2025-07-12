02ee78299aaaef39d3d19994c6d8cb20173266d5
Vee <vee1912566@gmail.com>
2025-07-12 15:53:47 +0530

    chore: ignore ts error

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -58,7 +58,7 @@ export default async function Page(props: {
     <DocsPage toc={page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <div className="flex flex-row gap-2 items-center border-b pt-2 pb-6">
-        {/*@ts-expect-error*/}
+        {/*@ts-expect-error: this library type is incorrect in v3*/}
         <LLMCopyButton slug={params.slug} />
         <ViewOptions markdownUrl={`${page.url}.mdx`} githubUrl={`check`} />
         <AISearchTrigger

4f0290456885979f6201ff4198a7e4e297a7e644
Vee <vee1912566@gmail.com>
2025-07-15 23:10:59 +0530

    chore: change claims section root - it's not root and remove hello builder as nav in sidebar

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,11 +1,15 @@
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
-import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
-import { source } from '@/lib/source';
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import { source } from "@/lib/source";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout {...baseOptions} tree={source.pageTree}>
+    <DocsLayout
+      {...baseOptions}
+      tree={source.pageTree}
+      sidebar={{ tabs: false }}
+    >
       {children}
     </DocsLayout>
   );
